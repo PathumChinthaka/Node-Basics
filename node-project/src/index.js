fe93fe8eb0e2ad0be9174d565cc0ebe1a75c76de
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-11-19 20:29:35 +0530

    send POST req

--- node-project/src/index.js
+++ node-project/src/index.js
@@ -1,7 +1,12 @@
 const express=require("express");
 
 const app=express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 const PORT=3000;
+
 
 app.listen(PORT,()=>{
   console.log("Server is Listning");
@@ -12,6 +17,6 @@ app.get('/',(req,res)=>{
 });
 
 app.post("/save",(req,res)=>{
-  console.log(req);
-  res.send("Ok Data Received")
+  console.log(req.body);
+  res.send(req.body);
 })
