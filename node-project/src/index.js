f983578c5757408f1dbb57dabfb52a2f6b6dabcb
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-11-19 21:11:10 +0530

    handle exception

--- node-project/src/index.js
+++ node-project/src/index.js
@@ -1,16 +1,13 @@
 const express=require("express");
-
 const app=express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+const mongoose=require("mongoose");
+mongoose.set('strictQuery',false);
+
 const PORT=3000;
-
-
-app.listen(PORT,()=>{
-  console.log("Server is Listning");
-});
 
 app.get('/',(req,res)=>{
   res.send("Hello Pathum");
@@ -19,4 +16,21 @@ app.get('/',(req,res)=>{
 app.post("/save",(req,res)=>{
   console.log(req.body);
   res.send(req.body);
-})
+});
+
+const start=async()=>{
+
+  try{
+
+    await mongoose.connect("mongodb+srv://pathum:KgQjzcmc1eZjOXOm@cluster0.hiwcmgf.mongodb.net/?retryWrites=true&w=majority");
+  
+    app.listen(PORT,()=>{
+      console.log("Server is Listning");
+    });
+
+  }catch(error){
+    console.log(error.message);
+  }
+}
+
+start();
